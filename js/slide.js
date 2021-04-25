1b0d325b588f55efa583c2403cbc7d3a621445ae
Sunset-Kim <wood2002@naver.com>
2021-04-25 12:49:39 +0900

    create project

---
+++ js/slide.js
@@ -0,0 +1,29 @@
+$('.banner-container').slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    customPaging: function(slider,i) {
+      return '<span class="pagination">' + $(slider.$slides[i]).find('.banner-slide').attr('data-dot-title') + '</span>';
+    },
+  });
+
+  $('.slide-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
