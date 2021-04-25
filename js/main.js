1468290569c95955c250feacc87bb62372699549
Sunset-Kim <wood2002@naver.com>
2021-04-25 14:27:46 +0900

    💄 update font icon - right arrow

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $('.banner-container').slick({
     infinite: true,
     arrows: false,
     dots: true,
+    autoplay: true,
     customPaging: function(slider,i) {
       return '<span class="pagination">' + $(slider.$slides[i]).find('.banner-slide').attr('data-dot-title') + '</span>';
     },
@@ -12,6 +13,7 @@ $('.banner-container').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     infinite: true,
+    autoplay: true,
     responsive: [
       {
         breakpoint: 1200,
