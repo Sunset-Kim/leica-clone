bb7ed39a9a79d0c0512a89165838cf89b6080b70
Sunset-Kim <wood2002@naver.com>
2021-04-27 18:05:28 +0900

    🚧 working page

---
+++ js/akademy/akademy.js
@@ -0,0 +1,32 @@
+$(function(){
+    // slide
+    $('.slide-container').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 786,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+          ]
+      });
+
+    //   scroll
+
+    
+});
